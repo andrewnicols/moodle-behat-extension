8c0114d13c7f066977f2d3997e7c20332eeb12d0
Andrew Nicols <andrew@nicols.co.uk>
2019-11-20 13:00:21 +0800

    Fix issue with SilverStripe WebDriver select implementation
    https://github.com/silverstripe/MinkFacebookWebDriver/pull/3

--- src/Moodle/BehatExtension/Driver/FacebookWebDriver.php
+++ src/Moodle/BehatExtension/Driver/FacebookWebDriver.php
@@ -17,8 +17,10 @@
 namespace Moodle\BehatExtension\Driver;
 
 use Behat\Mink\Exception\DriverException;
+use Behat\Mink\Selector\Xpath\Escaper;
 use Facebook\WebDriver\Remote\RemoteWebElement;
 use Facebook\WebDriver\WebDriverBy;
+use Facebook\WebDriver\WebDriverSelect;
 use SilverStripe\MinkFacebookWebDriver\FacebookWebDriver as SilverstripeFacebookWebDriver;
 
 /**
@@ -35,6 +37,11 @@ class FacebookWebDriver extends SilverstripeFacebookWebDriver
     protected static $currentBrowserName = '';
 
     /**
+     * @var Escaper
+     */
+    protected $xpathEscaper;
+
+    /**
      * Instantiates the driver.
      *
      * @param string $browserName Browser name
@@ -49,6 +56,7 @@ class FacebookWebDriver extends SilverstripeFacebookWebDriver
         parent::__construct($browserName, $desiredCapabilities, $wdHost);
 
         self::$currentBrowserName = $browserName;
+        $this->xpathEscaper = new Escaper();
     }
 
     /**
@@ -259,5 +267,151 @@ class FacebookWebDriver extends SilverstripeFacebookWebDriver
     {
         $script = 'window.Syn.trigger("' . $event . '", ' . $options . ', {{ELEMENT}})';
         $this->withSyn()->executeJsOnXpath($xpath, $script);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function selectOption($xpath, $value, $multiple = false)
+    {
+        $element = $this->findElement($xpath);
+        $tagName = strtolower($element->getTagName());
+
+        if ('input' === $tagName && 'radio' === strtolower($element->getAttribute('type'))) {
+            $this->selectRadioValue($element, $value);
+
+            return;
+        }
+
+        if ('select' === $tagName) {
+            $this->selectOptionOnElement($element, $value, $multiple);
+
+            return;
+        }
+
+        throw new DriverException(sprintf('Impossible to select an option on the element with XPath "%s" as it is not a select or radio input', $xpath));
+    }
+
+    /**
+     * Deselects all options of a multiple select
+     *
+     * Note: this implementation does not trigger a change event after deselecting the elements.
+     *
+     * @param RemoteWebElement $element
+     */
+    private function deselectAllOptions(RemoteWebElement $element)
+    {
+        $script = <<<JS
+var node = {{ELEMENT}};
+var i, l = node.options.length;
+for (i = 0; i < l; i++) {
+    node.options[i].selected = false;
+}
+JS;
+
+        $this->executeJsOnElement($element, $script);
+    }
+
+    /**
+     * @param RemoteWebElement $element
+     * @param string  $value
+     * @param bool    $multiple
+     */
+    protected function selectOptionOnElement(RemoteWebElement $element, $value, $multiple = false)
+    {
+        $escapedValue = $this->xpathEscaper->escapeLiteral($value);
+        // The value of an option is the normalized version of its text when it has no value attribute
+        $optionQuery = sprintf('.//option[@value = %s or (not(@value) and normalize-space(.) = %s)]', $escapedValue, $escapedValue);
+        $option = $this->findElement($optionQuery, $element);
+
+        if ($multiple || !$element->getAttribute('multiple')) {
+            if (!$option->isSelected()) {
+                $option->click();
+            }
+
+            return;
+        }
+
+        // Deselect all options before selecting the new one
+        $this->deselectAllOptions($element);
+        $option->click();
+    }
+
+    /**
+     * Selects a value in a radio button group
+     *
+     * @param RemoteWebElement $element An element referencing one of the radio buttons of the group
+     * @param string  $value   The value to select
+     *
+     * @throws DriverException when the value cannot be found
+     */
+    private function selectRadioValue(RemoteWebElement $element, $value)
+    {
+        // short-circuit when we already have the right button of the group to avoid XPath queries
+        if ($element->getAttribute('value') === $value) {
+            $element->click();
+
+            return;
+        }
+
+        $name = $element->getAttribute('name');
+
+        if (!$name) {
+            throw new DriverException(sprintf('The radio button does not have the value "%s"', $value));
+        }
+
+        $formId = $element->getAttribute('form');
+
+        try {
+            if (null !== $formId) {
+                $xpath = <<<'XPATH'
+//form[@id=%1$s]//input[@type="radio" and not(@form) and @name=%2$s and @value = %3$s]
+|
+//input[@type="radio" and @form=%1$s and @name=%2$s and @value = %3$s]
+XPATH;
+
+                $xpath = sprintf(
+                    $xpath,
+                    $this->xpathEscaper->escapeLiteral($formId),
+                    $this->xpathEscaper->escapeLiteral($name),
+                    $this->xpathEscaper->escapeLiteral($value)
+                );
+                $input = $this->findElement($xpath);
+            } else {
+                $xpath = sprintf(
+                    './ancestor::form//input[@type="radio" and not(@form) and @name=%s and @value = %s]',
+                    $this->xpathEscaper->escapeLiteral($name),
+                    $this->xpathEscaper->escapeLiteral($value)
+                );
+                $input = $this->findElement($xpath, $element);
+            }
+        } catch (NoSuchElementException $e) {
+            $message = sprintf('The radio group "%s" does not have an option "%s"', $name, $value);
+
+            throw new DriverException($message, 0, $e);
+        }
+
+        $input->click();
+    }
+
+    /**
+     * Executes JS on a given element - pass in a js script string and {{ELEMENT}} will
+     * be replaced with a reference to the element
+     *
+     * @example $this->executeJsOnXpath($xpath, 'return {{ELEMENT}}.childNodes.length');
+     *
+     * @param RemoteWebElement $element the webdriver element
+     * @param string  $script  the script to execute
+     * @param Boolean $sync    whether to run the script synchronously (default is TRUE)
+     *
+     * @return mixed
+     */
+    protected function executeJsOnElement(RemoteWebElement $element, $script, $sync = true)
+    {
+        $script  = str_replace('{{ELEMENT}}', 'arguments[0]', $script);
+        if ($sync) {
+            return $this->getWebDriver()->executeScript($script, [$element]);
+        }
+        return $this->getWebDriver()->executeAsyncScript($script, [$element]);
     }
 }
